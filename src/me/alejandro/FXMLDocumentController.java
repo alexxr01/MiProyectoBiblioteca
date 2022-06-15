d0f4cdc23c86aa3383a6a23aa42a269d14f530aa
usuario <usuario@DAM1PC13>
2022-06-15 19:43:33 +0200

    Comienzo del proyecto, clases basicas creadas.

---
+++ src/me/alejandro/FXMLDocumentController.java
@@ -0,0 +1,26 @@
+package me.alejandro;
+
+/**
+ * Controlador de FXML para el programa
+ * @author Alejandro Ruiz Bonillo
+ * @version 1.0
+ */
+
+import javafx.fxml.FXML;
+
+public class FXMLDocumentController {
+	
+	public FXMLDocumentController() {
+		
+	}
+	@FXML
+	private void initialize() {
+		
+	}
+	
+	@FXML
+	private void enviarDatos() {
+		this.enviarDatos();
+	}
+
+}
